425d88a8bedf330f71fc9d4a927d2abc1f2a75e3
Austin Pierce <adpierce74@yahoo.com>
2023-06-20 20:31:50 -0400

    Changed output to reflect correct public IP/DNS address for elastic ips

--- vm-output.tf
+++ vm-output.tf
@@ -2,11 +2,19 @@ output "vm_server_instance_id" {
   value = aws_instance.vm-server.id
 }
 output "vm_server_instance_public_dns" {
-  value = aws_instance.vm-server.public_dns
+  value = "${aws_eip_association.vm-eip-association.public_dns}"
 }
 output "vm_server_instance_public_ip" {
-  value = aws_instance.vm-server.public_ip
+  value = "${aws_eip_association.vm-eip-association.public_ip}"
 }
+
+#output "vm_server_instance_public_dns" {
+#  value = aws_instance.vm-server.public_dns
+#}
+#output "vm_server_instance_public_ip" {
+#  value = aws_instance.vm-server.public_ip
+#}
+
 output "vm_server_instance_private_ip" {
   value = aws_instance.vm-server.private_ip
 }

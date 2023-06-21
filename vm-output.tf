b963f096482a0c40ab260f39cbff6b973aee16dc
Austin Pierce <adpierce74@yahoo.com>
2023-06-20 20:34:46 -0400

    Changed output again to align with correct public elastic DNS

--- vm-output.tf
+++ vm-output.tf
@@ -5,7 +5,7 @@ output "vm_server_instance_public_dns" {
   value = "${aws_eip_association.vm-eip-association.public_dns}"
 }
 output "vm_server_instance_public_ip" {
-  value = "${aws_eip_association.vm-eip-association.public_ip}"
+  value = "${aws_eip.vm-eip-association.public_ip}"
 }
 
 #output "vm_server_instance_public_dns" {

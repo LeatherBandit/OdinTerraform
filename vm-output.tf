77d6bb019c718439a0f27184baf02ce271276361
Austin Pierce <adpierce74@yahoo.com>
2023-06-20 20:43:25 -0400

    Fixed reference to eip resourece instead of the eip_association

--- vm-output.tf
+++ vm-output.tf
@@ -2,10 +2,10 @@ output "vm_server_instance_id" {
   value = aws_instance.vm-server.id
 }
 output "vm_server_instance_public_dns" {
-  value = "${aws_eip_association.vm-eip-association.public_dns}"
+  value = "${aws_eip.vm-eip.public_dns}"
 }
 output "vm_server_instance_public_ip" {
-  value = "${aws_eip.vm-eip-association.public_ip}"
+  value = "${aws_eip_association.vm-eip-association.public_ip}"
 }
 
 #output "vm_server_instance_public_dns" {
